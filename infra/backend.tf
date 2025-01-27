b09fb5cd83814afbdd4fffb88500044b988ab9ba
muratariku0903 <18ba179r@rikkyo.ac.jp>
2025-01-27 23:39:22 +0900

    wip

---
+++ infra/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "summary-training-tf-state"
+    key            = "terraform/state/default.tfstate" # 状態ファイルの保存パス
+    region         = "ap-northeast-1"
+    encrypt        = true                             # サーバーサイド暗号化を有効
+  }
+}
