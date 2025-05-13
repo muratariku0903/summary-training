9ab0160291026e7cc07306c448d037dc7f0f9d78
muratariku0903 <18ba179r@rikkyo.ac.jp>
2025-05-13 19:28:53 +0900

    wip

--- infra/env/dev/main.tf
+++ infra/env/dev/main.tf
@@ -1,3 +1,6 @@
+# 以下リソース定義をコメントアウトするとtf.stateには存在するが定義がないのでdestroy処理が走るので注意
+# 監視から除外したい場合は、terraform state rm リソース名
+
 # 初期設定時、GitHub OIDCを信頼するIRMロールの作成
 module "iam" {
   source          = "../../modules/iam"
@@ -5,6 +8,7 @@ module "iam" {
   github_repo     = "summary-training"
   tf_state_bucket = "summary-training-tf-state"
 }
+
 
 
 # module "supabase" {
