450d7aea3179ab675763f749d16e7711f9b48ec5
muratariku0903 <18ba179r@rikkyo.ac.jp>
2025-07-02 01:47:05 +0900

    supabaseのサーバークライアンの初期化で、プロジェクト直下の環境変数を参照するように修正

--- frontend/src/lib/supabase/serverClient.ts
+++ frontend/src/lib/supabase/serverClient.ts
@@ -1,10 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import * as path from 'path'
-import * as dotenv from 'dotenv'
-
-// Supabase配下の.envファイルを読み込む
-// デフォルトだとNext.jsはプロジェクト直下の.envファイルを読み込む
-dotenv.config({ path: path.resolve(process.cwd(), 'supabase/.env') })
 
 const isDocGen = process.env.GENERATE_OPENAPI === 'true'
 
@@ -12,6 +6,6 @@ const isDocGen = process.env.GENERATE_OPENAPI === 'true'
 // 認証キー: process.env.SUPABASE_SERVICE_ROLE_KEY! を使用します。これは Supabase の Service Role Key (要するに管理者権限) であり、Supabase インスタンスに対する管理者レベルの権限を持っています。RLS をバイパスし、データベース内のすべてのデータにアクセスしたり、データの作成、更新、削除を自由に行うことができます。
 // セキュリティ: このキーは非常に強力なため、絶対にクライアントサイドに公開してはいけません。 もしクライアントサイドに漏洩した場合、悪意のあるユーザーがデータベースのすべてのデータを操作できてしまう可能性があります。
 export const serverClient = createClient(
-  isDocGen ? 'http://localhost:54321' : process.env.SUPABASE_URL!,
+  isDocGen ? 'http://localhost:54321' : process.env.NEXT_PUBLIC_SUPABASE_URL!,
   isDocGen ? 'dummy-anon-key-for-docs' : process.env.SUPABASE_SERVICE_ROLE_KEY!
 )
