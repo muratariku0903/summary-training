10487ad2f32f61d5cdada5dac01554488119921a
muratariku0903 <18ba179r@rikkyo.ac.jp>
2025-08-10 13:16:20 +0900

    test.sqlファイルを削除

--- frontend/supabase/seeds/test.sql
+++
@@ -1,6 +0,0 @@
--- supabase/seed.sql
-insert into public.test (email, full_name)
-values  ('alice@example.com',   'Alice A.'),
-        ('bob@example.com',     'Bob B.'),
-        ('carol@example.com',   'Carol C.'),
-        ('judy@example.com',    'Judy J.');
