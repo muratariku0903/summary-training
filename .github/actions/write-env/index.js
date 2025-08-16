01f6a09b3c6a106f38f956ca3d5b369e3bb1e0ee
muratariku0903 <18ba179r@rikkyo.ac.jp>
2025-08-17 02:24:04 +0900

    feat: E2E_SECRETのログ出力を削除し、コードを整理

--- .github/actions/write-env/index.js
+++ .github/actions/write-env/index.js
@@ -25,15 +25,9 @@ const mappings = [
   ["DESCOPE_JWKS_URI", "DESCOPE_JWKS_URI"],
   ["DESCOPE_MGMT_KEY", "DESCOPE_MGMT_KEY"],
 
-  ["E2E_ENABLED", "E2E_ENABLED"]
+  ["E2E_ENABLED", "E2E_ENABLED"],
   ["E2E_SECRET", "E2E_SECRET"]
 ];
-
-for (const [target, source] of mappings) {
-  if (target === 'E2E_SECRET') {
-    console.log('target: ', process.env[source])
-  }
-}
 
 const lines = mappings
   .map(([target, source]) => `${target}=${process.env[source] ?? ""}`)
