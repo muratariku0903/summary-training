8269dd52259dc052928b7928bb260c05f189341a
muratariku0903 <18ba179r@rikkyo.ac.jp>
2025-12-14 16:02:07 +0900

    fix: createBrowserClientにnoOpLockを追加し、ロック処理をスキップ

--- frontend/src/lib/supabase/client/browserClient.ts
+++ frontend/src/lib/supabase/client/browserClient.ts
@@ -1,10 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createBrowserClient } from '@supabase/ssr'
 
+const noOpLock = async <R>(
+  name: string,
+  acquireTimeout: number,
+  fn: () => Promise<R>,
+): Promise<R> => {
+  // ロック処理（navigator.locks）をスキップし、
+  // 渡された関数 fn を即座に実行して、その結果を返します
+  return await fn()
+}
+
 // 用途: 主にブラウザ環境（Webアプリケーションのフロントエンドなど）で使用されます。
 // 認証キー: process.env.SUPABASE_ANON_KEY! を使用します。これは Supabase の Anon Key (公開匿名キー) であり、サービスにアクセスする際の最低限の権限を提供します。このキーは誰でも利用できるように設計されており、データの読み取りなど、限定された操作のみを許可します。
 // セキュリティ: ブラウザで実行されるため、このクライアントを通じて行われる操作は Supabase の Row Level Security (RLS) ポリシーによって厳しく制限されます。これにより、意図しないデータアクセスや改ざんを防ぎます。
 export const browserClient = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      lock: noOpLock,
+    },
+  },
 )
