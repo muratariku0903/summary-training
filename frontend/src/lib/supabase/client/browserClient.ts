99f596477e8efbbe61d727a2480cd0a0183c4069
muratariku0903 <18ba179r@rikkyo.ac.jp>
2025-12-14 15:55:15 +0900

    fix: browserClientからauthオプションを削除

--- frontend/src/lib/supabase/client/browserClient.ts
+++ frontend/src/lib/supabase/client/browserClient.ts
@@ -7,9 +7,4 @@ import { createBrowserClient } from '@supabase/ssr'
 export const browserClient = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  {
-    auth: {
-      lock: async (fn: any) => await fn(),
-    },
-  },
 )
