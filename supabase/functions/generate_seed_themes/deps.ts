206cad36be919358ca2e7f11cab66be6f9e6421c
muratariku0903 <18ba179r@rikkyo.ac.jp>
2025-12-20 21:44:12 +0900

    refactor: getDrizzleDBClientのインポートパスを修正

--- supabase/functions/generate_seed_themes/deps.ts
+++ supabase/functions/generate_seed_themes/deps.ts
@@ -1,6 +1,6 @@
 import { pickRandomCategory } from '../_shared/repository/seed_generator_categories.ts'
 import { generateTheme } from '../_shared/usecase/generate_themes/generate_themes.ts'
-import { getDrizzleDBClient } from '../_shared/drizzle/client.ts'
+import { getDrizzleDBClient } from '../_shared/db/drizzle/client.ts'
 
 export const deps = {
   pickRandomCategory,
